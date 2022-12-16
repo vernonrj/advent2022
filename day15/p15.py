65bd48b6aed2b6776203c99a95e11c81a0e50b3a
Vernon Jones <vernonrjones@gmail.com>
2022-12-16 13:05:56 -0800

    day 15: cleanup

--- day15/p15.py
+++ day15/p15.py
@@ -4,7 +4,7 @@ import argparse
 from dataclasses import dataclass
 import itertools
 import re
-from typing import Any, Callable, Iterable, Iterator, Self
+from typing import Callable, Iterable, Iterator, Self
 
 INPUT_RE = re.compile(r"Sensor at x=(?P<sx>[-0-9]+), y=(?P<sy>[-0-9]+): closest beacon is at x=(?P<bx>[-0-9]+), y=(?P<by>[-0-9]+)")
 
@@ -53,7 +53,7 @@ def parse_input(lines: Iterable[str]) -> Iterator[Sensor]:
     return (Sensor.parse(s) for s in lines)
 
 def point_covered_by(point: Point, sensors: list[Sensor]) -> Iterator[Sensor]:
-    """return all sensors whose beacons are further away than `point`"""
+    """return all sensors whose beacons are further away than `point` i.e. whether their beacons "cover" this point"""
     return (s for s in sensors if s.closer_than_beacon(point))
 
 def apply_x(sensors: list[Sensor], func: Callable[[list[Point]], Point]) -> int:
@@ -61,19 +61,6 @@ def apply_x(sensors: list[Sensor], func: Callable[[list[Point]], Point]) -> int:
     points = (func(s.location, s.closest_beacon, key=lambda p: p.x) for s in sensors)
     applied_point = func(points, key=lambda p: p.x)
     return applied_point.x
-
-def points_covered_by_beacons(sensors: list[Sensor], y_val: int) -> int:
-    """return the number of points covered by beacons (excluding beacons themselves)"""
-    max_dist = max(Point.manhattan_distance(s.location, s.closest_beacon) for s in sensors)
-    min_x = apply_x(sensors, min) - max_dist
-    max_x = apply_x(sensors, max) + max_dist
-    print(f"min/max = {min_x}, {max_x}")
-    prange = list(Point.range_inclusive(Point(min_x, y_val), Point(max_x, y_val)))
-    beacons = set(prange) & {s.closest_beacon for s in sensors}
-    coverage = [(p, list(point_covered_by(p, sensors))) for p in prange]
-    covered_by_beacons = [p for (p, cov) in coverage if cov and p not in beacons]
-    # print(f"covered({len(covered_by_beacons)}): {covered_by_beacons}")
-    return len(covered_by_beacons)
 
 def around(point: Point, distance: int) -> Iterator[Point]:
     """returns an iterator of points `distance` away from `point`"""
@@ -87,21 +74,37 @@ def around(point: Point, distance: int) -> Iterator[Point]:
         if x and y:
             yield point + Point(-x, -y)
 
-def solve1(sensors: list[Sensor]):
-    c = points_covered_by_beacons(sensors, 2000000)
+def solve1(sensors: list[Sensor], y_val: int = 2000000):
+    """finds all points covered by beacons at a specific y-value"""
+    max_dist = max(Point.manhattan_distance(s.location, s.closest_beacon) for s in sensors)
+    min_x = apply_x(sensors, min) - max_dist
+    max_x = apply_x(sensors, max) + max_dist
+    print(f"min/max = {min_x}, {max_x}")
+    prange = list(Point.range_inclusive(Point(min_x, y_val), Point(max_x, y_val)))
+    beacons = set(prange) & {s.closest_beacon for s in sensors}
+    coverage = [(p, list(point_covered_by(p, sensors))) for p in prange]
+    covered_by_beacons = [p for (p, cov) in coverage if cov and p not in beacons]
+    # print(f"covered({len(covered_by_beacons)}): {covered_by_beacons}")
+    c = len(covered_by_beacons)
     print(f"covered by beacons: {c}")
 
 def solve2(sensors: list[Sensor], dim: int = 20):
+    """finds the only point not covered by a beacon in an area of [0, dim] by [0, dim]"""
     for each in sensors:
         print(f"sensor: {each}")
-        total_dist = Point.manhattan_distance(each.location, each.closest_beacon)
-        for point in around(each.location, total_dist+1):
+        beacon_distance = Point.manhattan_distance(each.location, each.closest_beacon)
+        for point in around(each.location, beacon_distance + 1):
             if point.x < 0 or point.x > dim or point.y < 0 or point.y > dim:
+                # point is outside the dimensions we're searching
                 continue
             cov = point_covered_by(point, sensors)
             try:
+                # we only need to check if this point is covered by _1 or more_ sensors,
+                # and it's theoretically cheaper to not find all sensors this point is covered by,
+                # so just try to get the first sensor/beacon that covers this point
                 next(cov)
             except StopIteration:
+                # no sensor/beacons cover this point. we've found it!
                 print(f"found uncovered point at {point}")
                 tuning_freq = point.x * 4000000 + point.y
                 print(f"tuning frequency: {tuning_freq}")
@@ -114,5 +117,5 @@ if __name__ == '__main__':
     matches = parser.parse_args()
     with open(matches.input) as fptr:
         sensors = list(parse_input(fptr.read().splitlines()))
-        # solve1(sensors)
+        solve1(sensors)
         solve2(sensors, dim=4000000)
